96f8fd406e04b90252f439480337511994e351dd
Brinda <brindasai.im19@rvce.edu.in>
2021-10-16 20:45:01 +0530

    Added program9.py

---
+++ program9.py
@@ -0,0 +1,16 @@
+
+# Bubble sort implementation using python
+def bubble_sort(sequence):
+     l=len(sequence)
+     for i in range (l-1,0,-1):
+         for j in range(i):
+             if sequence[j] > sequence[j+1]:
+                 temp=sequence[j]
+                 sequence[j]=sequence[j+1]
+                 sequence[j+1]=temp
+
+
+sequence = [9,4,3,7,1,5,2]
+print(bubble_sort(sequence))
+
+                 
